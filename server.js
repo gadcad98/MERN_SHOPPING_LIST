446e05add235a6ae29249f139305fbb169d0c827
gadcad98 <37885704+gadcad98@users.noreply.github.com>
2019-01-15 15:27:05 -0700

    updated database server

--- server.js
+++ server.js
@@ -7,3 +7,15 @@ const app = express();
 // Bodyparser middleware
 app.use(bodyParser.json());
 
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connet to Mongo
+mongoose
+    .connect(db, {userNewUrlParser: true})
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Sever started on port ${port}`));
