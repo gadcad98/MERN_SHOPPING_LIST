2fe43bbcc2f421a1967a60fc477c1815ac0550a4
gadcad98 <37885704+gadcad98@users.noreply.github.com>
2019-01-22 01:36:19 -0700

    added some CSS elements

--- routes/api/Items.js
+++ routes/api/Items.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const Item = require('../../models/Item');
 
 // @route   GET api/items
-// @desc    [desription] Get All items
+// @desc    [desription] Get All Items
 // @access  Public
 router.get('/', (req, res) => {
     Item.find()
